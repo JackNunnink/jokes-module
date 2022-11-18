d2b83a346dd750c46ce32a1884a4e96ef2e0ae41
Jack Nunnink <jack.nunnink@protonmail.com>
2022-11-18 10:58:11 -0500

    some clean up

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
 const getRandom = async () => {
-    let joke = null;
-
     const x = (Math.floor(Math.random() * 2) == 0);
     
     if (x) {
         const res = await axios.get('https://official-joke-api.appspot.com/random_joke');
-        joke = res.data;
+        return res.data;
     }
 
     const res = await axios.get('https://official-joke-api.appspot.com/jokes/random');
-    joke = res.data;
-
-    return joke;
+    return res.data;
 }
 
 export default getRandom;
