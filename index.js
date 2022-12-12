d4e09f0eea9e76780a714721c54b95270107dc7e
Jack Nunnink <jack.nunnink@protonmail.com>
2022-12-12 16:35:56 -0500

    wip

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import axios from "axios";
 
 const getRandom = async () => {
+    const options = {
+        method: 'GET',
+        url: 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random',
+        headers: {
+            accept: 'application/json',
+            'X-RapidAPI-Key': '2d42e62b44mshd5b7056f6335843p11e6a0jsn41ca8f6b6f97',
+            'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
+        }
+    };
+
     const x = (Math.floor(Math.random() * 2) == 0);
     
     if (x) {
-        const res = await axios.get('https://official-joke-api.appspot.com/random_joke');
-        return res.data;
+        await axios.request(options)
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     }
 
-    const res = await axios.get('https://official-joke-api.appspot.com/jokes/random');
+    const res = await axios.get('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random');
     return res.data;
 }
 
