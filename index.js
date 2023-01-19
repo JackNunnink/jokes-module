0b05dcd974ab46ba92277e05c339e0a5d85176bb
Jack Nunnink <jack.nunnink@protonmail.com>
2023-01-19 17:03:11 -0500

    bug fix

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 const getRandom = async () => {
-
-    const x = (Math.floor(Math.random() * 2) == 0);
+    const x = Math.random() < 0.5;
     
-    if (x) {
+    if (x === false) {
         const res = await axios.get('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', {
             headers: {
                 accept: 'application/json',
                 'X-RapidAPI-Key': '2d42e62b44mshd5b7056f6335843p11e6a0jsn41ca8f6b6f97',
             }
         });
-        console.log(res.data)
+        console.log(x)
         return res.data;
     }
-
-    const res = await axios.get('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random');
+    
+    const res = await axios.get('https://official-joke-api.appspot.com/random_joke');
+    console.log(x);
     return res.data;
 }
 
